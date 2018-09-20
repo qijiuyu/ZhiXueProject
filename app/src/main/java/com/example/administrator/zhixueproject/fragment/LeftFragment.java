b1b036e31669e6dea31b452c14e4b38c16eb3251
Petergee <qipeng.ge@edaibu.net>
2018-09-20 18:17:14 +0800

    侧边栏部分代码

--- app/src/main/java/com/example/administrator/zhixueproject/fragment/LeftFragment.java
+++ app/src/main/java/com/example/administrator/zhixueproject/fragment/LeftFragment.java
@@ -2,32 +2,83 @@ package com.example.administrator.zhixueproject.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.administrator.zhixueproject.R;
+import com.example.administrator.zhixueproject.adapter.CollegeNameAdapter;
+import com.example.administrator.zhixueproject.application.MyApplication;
+import com.example.administrator.zhixueproject.bean.Colleges;
+import com.example.administrator.zhixueproject.bean.UserBean;
+import com.example.administrator.zhixueproject.bean.UserInfo;
+
+import java.util.List;
+
+import retrofit2.http.POST;
 
 /**
  * 侧边栏
  * Created by Administrator on 2018/1/3 0003.
  */
 
-public class LeftFragment extends BaseFragment{
+public class LeftFragment extends BaseFragment implements BaseQuickAdapter.OnItemClickListener {
+
+    private CollegeNameAdapter mAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
 
-    View view=null;
+    private View view=null;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_left_menu, container, false);
+        initView();
         return view;
     }
 
+    private void initView() {
+        ImageView imgHead= (ImageView) view.findViewById(R.id.iv_menu_head);
+        TextView tvName= (TextView) view.findViewById(R.id.tv__menu_name);
+        TextView tvSign= (TextView) view.findViewById(R.id.tv_meun_sign);
+        // recyclerView
+        RecyclerView mRecyclerView= (RecyclerView) view.findViewById(R.id.rv_college_name);
+        LinearLayoutManager layout = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layout);
+        UserInfo userInfo=MyApplication.userInfo;
+        if (userInfo==null){
+            return;
+        }
+        UserBean bean=userInfo.getData().getUser();
+        // 头像
+        Glide.with(MyApplication.application).
+                load(bean.getUserImg()).
+                into(imgHead);
+        // 昵称
+        tvName.setText(bean.getUserName());
+        // 简介
+        tvSign.setText(bean.getUserIntro());
+        // adapter 数据
+        List<Colleges> dataBean=userInfo.getData().getColleges();
+        mAdapter = new CollegeNameAdapter(R.layout.joined_college_item, dataBean);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(this);
+
+    }
 
 
+    @Override
+    public void onItemClick(BaseQuickAdapter baseQuickAdapter, View view, int position) {
+        Colleges colleges=mAdapter.getData().get(position);
+        // 设置title
+    }
 }
