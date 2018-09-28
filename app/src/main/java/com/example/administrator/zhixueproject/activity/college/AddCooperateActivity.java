3d741c8c1e1fbf202a7ea791a1d08490aa386752
qijiuyu79 <jiuyu.qi@edaibu.net>
2018-09-28 16:09:50 +0800

    开发学院管理

--- app/src/main/java/com/example/administrator/zhixueproject/activity/college/AddCooperateActivity.java
+++ app/src/main/java/com/example/administrator/zhixueproject/activity/college/AddCooperateActivity.java
@@ -1,12 +1,13 @@
 package com.example.administrator.zhixueproject.activity.college;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import com.example.administrator.zhixueproject.R;
 import com.example.administrator.zhixueproject.activity.BaseActivity;
 import com.example.administrator.zhixueproject.fragment.college.TopicListFragment;
@@ -19,10 +20,13 @@ public class AddCooperateActivity extends BaseActivity implements View.OnClickLi
     private EditText etName,etTimeNum;
     private TextView tvTopic,tvTeacherName;
     private TopicListFragment topicListFragment;
+    //侧滑菜单
+    public static DrawerLayout mDrawerLayout;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cooperate);
         initView();
+        rightMenu();
     }
 
 
@@ -36,6 +40,7 @@ public class AddCooperateActivity extends BaseActivity implements View.OnClickLi
         tvTopic=(TextView)findViewById(R.id.tv_topic_content);
         tvTeacherName=(TextView)findViewById(R.id.tv_teacher_name);
         etTimeNum=(EditText)findViewById(R.id.et_purchase_period);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         findViewById(R.id.rl_add_topic).setOnClickListener(this);
         findViewById(R.id.rl_choose_teacher).setOnClickListener(this);
         findViewById(R.id.tv_setting_save).setOnClickListener(this);
@@ -46,7 +51,7 @@ public class AddCooperateActivity extends BaseActivity implements View.OnClickLi
         switch (v.getId()){
             //所选话题
             case R.id.rl_add_topic:
-                 showTopicFragment(true);
+                 openRight();
                  break;
             //发布人
             case R.id.rl_choose_teacher:
@@ -61,23 +66,36 @@ public class AddCooperateActivity extends BaseActivity implements View.OnClickLi
 
 
     /**
-     * 显示话题弹窗
-     *
-     * @param show
+     * 打开侧边栏
      */
-    private void showTopicFragment(boolean show) {
-        if (topicListFragment == null) {
-            topicListFragment = new TopicListFragment();
-        }
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if (show) {
-            //防止快速点击
-            if (topicListFragment.isAdded()) {
-                return;
+    public static void openRight() {
+        mDrawerLayout.openDrawer(Gravity.RIGHT);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, Gravity.LEFT);
+    }
+
+    /**
+     * 设置侧边栏
+     */
+    private void rightMenu() {
+        // 设置遮盖主要内容的布颜色
+        mDrawerLayout.setScrimColor(Color.TRANSPARENT);
+        //关闭手势滑动
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+            public void onDrawerStateChanged(int arg0) {
             }
-            transaction.add(R.id.fl_topic, topicListFragment).commit();
-        } else {
-            transaction.remove(topicListFragment).commit();
-        }
+
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                View content = mDrawerLayout.getChildAt(0);
+                int offset = (int) (drawerView.getWidth() * slideOffset);
+                content.setTranslationX(offset);
+            }
+
+            public void onDrawerOpened(View arg0) {
+            }
+
+            public void onDrawerClosed(View arg0) {
+            }
+        });
     }
 }
