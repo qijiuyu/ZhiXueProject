8abff873da0bd6c22e207df7260ff845c23d8f86
qijiuyu79 <qijiuyu@bjgoodwill.com>
2019-12-17 11:20:11 +0800

    修改

--- app/src/main/java/com/example/administrator/zhixueproject/utils/UpdateVersionUtils.java
+++ app/src/main/java/com/example/administrator/zhixueproject/utils/UpdateVersionUtils.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -146,10 +148,21 @@ public class UpdateVersionUtils {
                             dialog.dismiss();
                             File file = new File(savePath);
                             if (file.isFile()) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW);
-                                intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-                                intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
-                                mContext.startActivity(intent);
+                                try {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                                    Uri uri;
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { // 适配Android 7系统版本
+                                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //添加这一句表示对目标应用临时授权该Uri所代表的文件
+                                        uri = FileProvider.getUriForFile(mContext, mContext.getPackageName() + ".fileprovider", file);//通过FileProvider创建一个content类型的Uri
+                                    } else {
+                                        uri = Uri.fromFile(file);
+                                    }
+                                    intent.setDataAndType(uri, "application/vnd.android.package-archive"); // 对应apk类型
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    mContext.startActivity(intent);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         }
                     }, 1000);
